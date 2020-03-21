f11bb948142a883e758c1eb2d6906017240b17b0
991688344 <42809317+991688344@users.noreply.github.com>
2020-03-21 11:53:24 +0800

    Update xss-img.js

--- xss-img.js
+++ xss-img.js
@@ -1,5 +1,6 @@
 <script>
-var img=document.createElement("img");
-img.src="https://ns-strategy.cdn.bcebos.com/ns-strategy/upload/fc_big_pic/part-00391-3849.jpg";
-document.body.appendChild("img");
+  alert("from github");
+  var img=document.createElement("img");
+  img.src="https://ns-strategy.cdn.bcebos.com/ns-strategy/upload/fc_big_pic/part-00391-3849.jpg";
+  document.body.appendChild("img");
 </script>
