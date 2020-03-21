5d25e4d069f8d461033df1764512eb782e38902f
991688344 <42809317+991688344@users.noreply.github.com>
2020-03-21 11:46:50 +0800

    Update xss-img.js

--- xss-img.js
+++ xss-img.js
@@ -1,3 +1,5 @@
+<script>
 var img=document.createElement("img");
 img.src="https://ns-strategy.cdn.bcebos.com/ns-strategy/upload/fc_big_pic/part-00391-3849.jpg";
 document.body.appendChild("img");
+</script>
